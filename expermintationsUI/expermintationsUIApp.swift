32baefed4d40c49e999231eb4f27981032c26389
Saqer Alshehri <riche.homme.10@gmail.com>
2026-03-22 04:12:16 +0300

    first commit

---
+++ expermintationsUI/expermintationsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  expermintationsUIApp.swift
+//  expermintationsUI
+//
+//  Created by Saqer Alshehri on 03/10/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct expermintationsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
